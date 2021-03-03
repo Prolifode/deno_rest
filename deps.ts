3244a93ec09cfc614ad193dd3778e0ad6edd9f39
Vicky Gonsalves <vicky.gonsalves@outlook.com>
2021-03-03 13:36:04 +0530

    Updated to deno 1.8

--- deps.ts
+++ deps.ts
@@ -13,9 +13,9 @@ export {
 export type { RouterContext } from "https://deno.land/x/oak/mod.ts";
 export { config as dotEnv } from "https://deno.land/x/dotenv/mod.ts";
 export { getLogger, handlers, setup } from "https://deno.land/std/log/mod.ts";
-export { MongoClient } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/refactor/mod.ts"; // Used directly from refactor branch
-export type { Document } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/refactor/mod.ts"; // Used directly from refactor branch
-export { ObjectId } from "https://raw.githubusercontent.com/manyuanrong/deno_mongo/refactor/bson/mod.ts"; // Used directly from refactor branch
+export { MongoClient } from "https://deno.land/x/mongo/mod.ts";
+export type { Document } from "https://deno.land/x/mongo/mod.ts";
+export { ObjectId } from "https://deno.land/x/mongo/bson/mod.ts";
 export { oakCors } from "https://deno.land/x/cors/mod.ts";
 export type { Header, Payload } from "https://deno.land/x/djwt/mod.ts";
 export { create, verify } from "https://deno.land/x/djwt/mod.ts";
